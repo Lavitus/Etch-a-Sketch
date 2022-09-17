b15524560ebf2325502b742408977361cd08ae4e
LavituScuperino <samet71simsek@gmail.com>
2022-09-18 01:42:51 +0200

    Visuals and custom size
    -Custom canvas size from 16-100
    
    -Keep track of current size
    
    -Currently only painting in color red on mouse over,
    click and hold planned.
    
    -Update/Reset button visually changing when
    input value changes to different size or current size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,64 @@
-let sketchArea = document.getElementById("sketchArea");
-sketchField = 16 * 16;
+// grid/canvas area 
+let canvas = document.getElementById("canvas");
+// standard canvas size
+let currentCanvasVal = 16;
 
-for (let i = 0; i < sketchField; i++) {
-    let sketchBox = document.createElement("div");
-    sketchBox.setAttribute("id", "sketchBox");
-    
-    sketchBox.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "red";
-    }); 
-    
-    sketchArea.appendChild(sketchBox);
+let canvasSizeText = document.getElementById("canvasCurrentSize");
+//set & update text that showcases the current canvas size
+function canvasTextUpdater (){
+    canvasSizeText.textContent = `Current size ${currentCanvasVal}x${currentCanvasVal}`  
+}
+canvasTextUpdater();
+
+updateBtnText = document.getElementById("updCanvasTxt")
+
+// input for canvas size getting saved to variable everytime the value changes
+let newCanvasInput = 16;
+let canvasInput = document.getElementById("canvasSizeInput");
+canvasInput.addEventListener("input", (event) => {
+    newCanvasInput = document.getElementById("canvasSizeInput").value;
+    if (newCanvasInput <= 100) {
+        if (newCanvasInput == currentCanvasVal) {
+            canvasUpdater.classList.remove("rdyGreen");
+            updateBtnText.innerText = "Reset";
+        } else {
+            canvasUpdater.classList.add("rdyGreen");
+            updateBtnText.innerText = "Update";
+        }
+    }
+});
+// update btn click starts updateCanvas function
+let canvasUpdater = document.getElementById("updCanvas");
+canvasUpdater.addEventListener("click",
+    function() {
+        if (newCanvasInput <= 100) {
+            canvasUpdater.classList.remove("rdyGreen");
+            updateBtnText.innerText = "Reset";
+            updCanvas();
+        }
+    });
+// removes current boxes, get new grid(canvas) size value from input and 
+function updCanvas() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+      }
+    currentCanvasVal = newCanvasInput;
+    loadCanvas();
 }
 
+// applies grid size & loads canvas on pageload(or when called with the update button)
+function loadCanvas() {
+    canvas.style.cssText = `grid-template-columns: repeat(${currentCanvasVal}, 2fr);grid-template-rows: repeat(${currentCanvasVal}, 2fr);`;
+    for (let i = 0; i < currentCanvasVal * currentCanvasVal; i++) {
+        let canvasBox = document.createElement("div");
+        canvasBox.setAttribute("id", "canvasBox");
+        
+        canvasBox.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "red";
+        }); 
+        
+        canvas.appendChild(canvasBox);
+    }
+    canvasTextUpdater()
+}
+loadCanvas();
--- readme.txt
+++ readme.txt
@@ -1,1 +1,6 @@
+Etch-a-Sketch
 
+Custom canvas size
+Keep track of current size
+Currently only painting in color red on mouse over, click and hold planned.
+Update/Reset button visually changing when input value changes to different size or current size
