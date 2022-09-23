39cbec045c18a48ab60fbeca8c56146ff428fd2f
LavituScuperino <samet71simsek@gmail.com>
2022-09-23 22:24:59 +0200

    Color changeable now

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,25 @@
-// grid/canvas area 
-let canvas = document.getElementById("canvas");
-// standard canvas size
-let currentCanvasVal = 16;
+const canvas = document.getElementById("canvas");
+const canvasSizeText = document.getElementById("canvasCurrentSize");
+const updateBtnText = document.getElementById("updCanvasTxt");
+const canvasInput = document.getElementById("canvasSizeInput");
+const canvasUpdater = document.getElementById("updCanvas");
+const colorChoices = document.getElementById('colorSelection');
 
-let canvasSizeText = document.getElementById("canvasCurrentSize");
-//set & update text that showcases the current canvas size
+let currentCanvasVal = 16;
+let newCanvasInput = 16;
+let currentColorChoice = "white";
+let isMouseDown;
+
+document.addEventListener("mousedown", () => isMouseDown = true);
+document.addEventListener("mouseup", () => isMouseDown = false);  
+
+// update text that showcases the current canvas size
 function canvasTextUpdater (){
     canvasSizeText.textContent = `Current size ${currentCanvasVal}x${currentCanvasVal}`  
 }
 canvasTextUpdater();
 
-updateBtnText = document.getElementById("updCanvasTxt")
-
 // input for canvas size getting saved to variable everytime the value changes
-let newCanvasInput = 16;
-let canvasInput = document.getElementById("canvasSizeInput");
 canvasInput.addEventListener("input", (event) => {
     newCanvasInput = document.getElementById("canvasSizeInput").value;
     if (newCanvasInput <= 100) {
@@ -26,39 +31,61 @@ canvasInput.addEventListener("input", (event) => {
             updateBtnText.innerText = "Update";
         }
     }
-});
-// update btn click starts updateCanvas function
-let canvasUpdater = document.getElementById("updCanvas");
+})
+
+// update/reset button resets button text & color, then run function to remove canvas boxes
 canvasUpdater.addEventListener("click",
     function() {
         if (newCanvasInput <= 100) {
             canvasUpdater.classList.remove("rdyGreen");
             updateBtnText.innerText = "Reset";
-            updCanvas();
+            removeCanvas();
         }
-    });
-// removes current boxes, get new grid(canvas) size value from input and 
-function updCanvas() {
+    })
+
+    // removes current boxes, get new grid(canvas) size value from input, then run function to load new boxes
+function removeCanvas() {
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
       }
     currentCanvasVal = newCanvasInput;
     loadCanvas();
-}
+};
 
 // applies grid size & loads canvas on pageload(or when called with the update button)
 function loadCanvas() {
     canvas.style.cssText = `grid-template-columns: repeat(${currentCanvasVal}, 2fr);grid-template-rows: repeat(${currentCanvasVal}, 2fr);`;
     for (let i = 0; i < currentCanvasVal * currentCanvasVal; i++) {
         let canvasBox = document.createElement("div");
-        canvasBox.setAttribute("id", "canvasBox");
         
-        canvasBox.addEventListener("mouseover", (event) => {
-            event.target.style.backgroundColor = "red";
-        }); 
+        canvasBox.setAttribute("class", "canvasBox");
+        canvasBox.setAttribute("id", "white");
+        
+        canvasBox.addEventListener('dragstart', (e) => {
+            e.preventDefault()
+          })
+          
+          canvasBox.addEventListener('drop', (e) => {
+            e.preventDefault()
+          })
+        
+        canvasBox.addEventListener("mouseover", (e) => changeColor(e)) 
+        canvasBox.addEventListener("click", (e) => changeColor(e)); 
         
         canvas.appendChild(canvasBox);
     }
     canvasTextUpdater()
 }
 loadCanvas();
+
+// get color ID on click
+colorChoices.addEventListener("click", 
+    function(event) {
+        let colorIndex = Array.prototype.indexOf.call(colorChoices.children, event.target);
+        currentColorChoice = colorChoices.children[colorIndex].id;
+    });
+// change color when clicking and mouse held down while hovering over boxes
+function changeColor(e) {
+    if(!isMouseDown) return;
+    e.target.setAttribute("id", currentColorChoice)
+}
--- readme.txt
+++ readme.txt
@@ -2,5 +2,5 @@ Etch-a-Sketch
 
 Custom canvas size
 Keep track of current size
-Currently only painting in color red on mouse over, click and hold planned.
++ Colors changeable now
 Update/Reset button visually changing when input value changes to different size or current size
